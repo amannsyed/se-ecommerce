2c165d7ac74e237311314cb204fb04c93909f625
amannsyed <78953449+amannsyed@users.noreply.github.com>
2022-04-19 14:07:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Infi Mart | All Rights Reserved | Contact Us: +91 9876543210</b></span></div>
+</footer>
+    
